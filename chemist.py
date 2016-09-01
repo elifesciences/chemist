0477dac3abd93b805978806906b95c7c4806fc7e
Giorgio Sironi <g.sironi@elifesciences.org>
2016-09-01 12:32:50 +0100

    Introducing logging

--- chemist.py
+++ chemist.py
@@ -1,4 +1,5 @@
 import json
+import logging
 import pprint
 import web
 
@@ -6,12 +7,21 @@ urls = (
     '/github-hooks', 'GithubHooks'
 )
 
+LOG = logging.getLogger(__name__)
+FORMAT = "[%(asctime)-15s][%(levelname)s] %(message)s"
+LOG.setLevel(logging.INFO)
+handler = logging.StreamHandler()
+handler.setFormatter(logging.Formatter(FORMAT))
+LOG.addHandler(handler)
+
 class GithubHooks:
     def POST(self):
         content = json.loads(web.data())
-        pprint.pprint(content)
+        repository = content['repository']['full-name']
+        LOG.info('Received push hook for %s' % repository)
         return ''
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
+    LOG.info('Started')
     app.run()
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-exec venv/bin/python chemist.py
+exec venv/bin/python chemist.py $*
