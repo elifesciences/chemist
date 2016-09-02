6392c600463fb3e16322d66004b806f1a1aa2b16
Giorgio Sironi <g.sironi@elifesciences.org>
2016-09-02 09:43:37 +0100

    Added signature check

--- chemist.py
+++ chemist.py
@@ -1,6 +1,8 @@
 from collections import OrderedDict
 from getopt import getopt
+from hashlib import sha1
 from subprocess import call
+import hmac
 import json
 import logging
 import re
@@ -25,13 +27,27 @@ try:
     options = OrderedDict(config.items('chemist'))
     repositories = options['repositories'].split(',')
     command = options['command']
+    secret = options['secret']
 except:
     LOG.exception("Please create a app.cfg file.")
     sys.exit(-1)
 
+def verify_signature(body, signature, secret):
+    expected_signature = str(hmac.new(str(secret), msg=body, digestmod=sha1).hexdigest())
+    # Using a plain == operator is not advised, see:
+    # https://developer.github.com/webhooks/securing/
+    # however, Python prior to 2.7.7 doesn't have hmac.compare_digest
+    # and we have Python 2.7.6
+    return expected_signature == signature
+
 class GithubHooks:
     def POST(self):
-        content = json.loads(web.data())
+        signature = web.ctx.env.get('X_HUB_SIGNATURE')
+        body = web.data()
+        if not verify_signature(body, signature, secret):
+            LOG.error('Refused signed (%s) request: %s', signature, body)
+            return web.webapi.forbidden()
+        content = json.loads(body)
         repository = content['repository']['full_name']
         LOG.info('Received push hook for `%s`' % repository)
         for interesting in repositories:
@@ -45,4 +61,5 @@ if __name__ == "__main__":
     app = web.application(urls, globals())
     LOG.info('Started with repositories %s', repositories)
     LOG.info('Started with command `%s`', command)
+    LOG.info('Started with secret `%s`', re.sub(r'.', '*', secret))
     app.run()
---
+++ test_chemist.py
@@ -0,0 +1,11 @@
+import chemist
+import unittest
+
+class ChemistTest(unittest.TestCase):
+    def test_security_validation(self):
+        body = '{"key":"value"}'
+        signature = "a4b19954c9b3e8e0ef1b7c993fcff7e2a2564bf6"
+        secret = "token_stored_in_github_webhook_configuration"
+        self.assertTrue(chemist.verify_signature(body, signature, secret))
+        self.assertFalse(chemist.verify_signature(body, 'plain wrong signature', secret))
+
